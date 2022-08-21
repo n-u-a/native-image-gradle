567592bf52ebcc7e2aa4c6113451b3e1f368b80e
n-u-a <freizeit.g@gmail.com>
2022-08-21 16:30:56 +0900

    認証方法を修正

--- demo/src/main/java/com/example/demo/lambdas/SelectUserFunction.java
+++ demo/src/main/java/com/example/demo/lambdas/SelectUserFunction.java
@@ -3,6 +3,8 @@ package com.example.demo.lambdas;
 import com.example.demo.dao.UserDao;
 import com.example.demo.domain.UserDomain;
 import com.example.demo.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.Message;
@@ -23,11 +25,15 @@ public class SelectUserFunction implements Function<Message<Person>, Message<Per
     @Autowired
     private UserDao sampleDao;
 
+    private static final Logger logger = LoggerFactory.getLogger(SelectUserFunction.class);
+
     @Override
     public Message<Person> apply(Message<Person> request) {
         Person person = request.getPayload();
         Map<String, Object> resultHeader = new HashMap();
         resultHeader.put("statusCode", HttpStatus.OK.value());
+
+        logger.info("sample log");
 
         UserDomain userDomain = sampleDao.getUser(person.getId());
 
